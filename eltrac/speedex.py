708b1d72bfa01667ffeea83b781137aeaf85baf7
gntouts <ece8441@upnet.gr>
2020-12-23 15:09:55 +0200

    'p'

--- eltrac/speedex.py
+++ eltrac/speedex.py
@@ -18,6 +18,19 @@ class SpeedexOrder:
         else:
             soup = BeautifulSoup(requests.get(self.url).text, "lxml")
             updates = soup.find_all(attrs={"class": "timeline-item"})
+            final = ''
+
+            final = soup.find(attrs={"class":"delivered-speedex"})
+            
+            if final!='':
+                tremp = final.find('p').text
+                timespace = final.find(
+                    attrs={"class": "font-small-3"}).get_text()
+                time = timespace.split(',')[-1].strip()
+                space = timespace.split(',')[0].strip()
+                final = {'status': tremp, 'time': time, 'space': space, 'datetime':datetime.datetime.strptime(
+                    time, '%d/%m/%Y στις %H:%M')}
+            self.final = final
             steps = []
             for update in updates:
                 temp = {}
@@ -35,4 +48,6 @@ class SpeedexOrder:
             if steps == []:
                 self.result = 'No data'
             else:
-                self.result = steps
+                self.result = steps.append(self.final)
+
+new = SpeedexOrder('700005668380')
