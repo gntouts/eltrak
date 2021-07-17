d3d508caad810a55757d6fa7bb1a540e3f098ea5
gntouts <g.ntouts@outlook.com>
2021-07-17 13:58:05 +0300

    added deprecation support

---
+++ EltrakLib/deprecation_support.py
@@ -0,0 +1,16 @@
+from EltrakLib.BaseClasses import TrackingResult, TrackingCheckpoint
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass
+class DeprecatedTrackingResult:
+    courier: str
+    tracking: str
+    updates: List[TrackingCheckpoint]
+
+    @classmethod
+    def from_result(cls, tracking_result: TrackingResult):
+        deprecated = cls(tracking_result.courier,
+                         tracking_result.tracking_number, tracking_result.updates)
+        return deprecated
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from EltrakLib.TrackerFactory import get_factory, CourierNotSupportedError
 from EltrakLib.BaseClasses import InvalidTrackingNumber
+from EltrakLib.deprecation_support import DeprecatedTrackingResult
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from apifunctions import getACS, getElta, getSpeedex, getGeniki
@@ -59,40 +60,10 @@ def track_courier(courier: CourierName, tracking_number: str):
         )
 
 
-@ app.get("/v1/track/speedex/{tracking}", tags=["v1"], deprecated=True)
-def trackSpeedex(tracking):
-    """Tracks Speedex vouchers 
-
-    Parameters:
-    tracking (str): Tracking number
-   """
-    response = getSpeedex(tracking=tracking)
-    return response
+# ------------V1-------------
 
 
-@ app.get("/v1/track/acs/{tracking}", tags=["v1"], deprecated=True)
-def trackACS(tracking):
-    """Tracks ACS vouchers 
-
-    Parameters:
-    tracking (str): Tracking number
-   """
-    response = getACS(tracking=tracking)
-    return response
-
-
-@ app.get("/v1/track/elta/{tracking}", tags=["v1"], deprecated=True)
-def trackElta(tracking):
-    """Tracks Elta Courier vouchers 
-
-    Parameters:
-    tracking (str): Tracking number
-   """
-    response = getElta(tracking=tracking)
-    return response
-
-
-@ app.get("/v1/track/geniki/{tracking}", tags=["v1"], deprecated=True)
+@ app.get("/v1/track/geniki/{tracking}", tags=["v1"])
 def trackGeniki(tracking):
     """Tracks Geniki Taxidromiki Courier vouchers 
 
@@ -101,3 +72,31 @@ def trackGeniki(tracking):
    """
     response = getGeniki(tracking=tracking)
     return response
+
+
+@app.get('/v1/track/{courier}/{tracking_number}', tags=["v1"], deprecated=True)
+def deprecated_track_courier(courier: CourierName, tracking_number: str):
+    try:
+        courier = str(courier).split('.')[-1]
+        factory = get_factory(courier)
+        tracker = factory.get_tracker()
+        result = tracker.track(tracking_number)
+        return DeprecatedTrackingResult().from_result(result)
+    except InvalidTrackingNumber as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.message,
+            headers={"X-Error": "InvalidTrackingNumber"},
+        )
+    except CourierNotSupportedError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=e.message,
+            headers={"X-Error": "CourierNotSupportedError"},
+        )
+    except:
+        raise HTTPException(
+            status_code=500,
+            detail='Unknow error. Please provide more details for debugging.',
+            headers={"X-Error": "UnkownError"},
+        )
