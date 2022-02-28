c5126f2235228329c9831deb271a6a9201c5e07a
George Ntoutsos <g.ntouts@outlook.com>
2022-02-28 22:51:51 +0000

    removed unused import&code, fixed docstring

--- src/EltrakLib/ACS.py
+++ src/EltrakLib/ACS.py
@@ -1,18 +1,18 @@
 from EltrakLib.BaseClasses import InvalidTrackingNumber, CourierTracker, TrackingCheckpoint, TrackingResult, format_timestamp
-from requests import post, get
+from requests import get
 from string import digits
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 
 class AcsTracker(CourierTracker):
-    """Tracker object for Speedex tracking numbers`"""
+    """Tracker object for ACS tracking numbers`"""
     courier = 'acs'
     base_url = 'https://api.acscourier.net/api/parcels/search/'
     allowed = digits
 
     def sanitize(self, tracking_number: str) -> str:
-        '''Attempts to sanitize the given tracking number according to ELTA format'''
+        '''Attempts to sanitize the given tracking number according to ACS format'''
         new = ''.join([i for i in str(tracking_number)
                       if i in self.allowed])
         if len(new) != 10:
@@ -29,28 +29,29 @@ class AcsTracker(CourierTracker):
         '''Parses the results into useable information'''
 
         def parse_checkpoint(checkpoint):
-            try: 
+            try:
                 if checkpoint['info'] == ' ':
                     status = checkpoint['description']
                 else:
-                    status = checkpoint['description'] + ' - ' + checkpoint['info']
+                    status = checkpoint['description'] + \
+                        ' - ' + checkpoint['info']
             except:
-                status = checkpoint['description'] 
+                status = checkpoint['description']
             timestamp = checkpoint['controlPointDate'].split('.')[0]
             time = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
             time = time.strftime('%d/%m/%Y στις %H:%M')
             space = checkpoint['controlPoint']
             return TrackingCheckpoint(status, time, space,
-                format_timestamp(datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')))
+                                      format_timestamp(datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')))
 
-        updates = [parse_checkpoint(update) for update in tracking_info['statusHistory']]
-        last = sorted(updates, key=lambda k: k.datetime)[0]
+        updates = [parse_checkpoint(update)
+                   for update in tracking_info['statusHistory']]
         return TrackingResult(
             courier='ACS',
             tracking_number=tracking_info['trackingNumber'],
             updates=updates,
             delivered=tracking_info['isDelivered']
-            )
+        )
 
     def track(self, tracking_number: str):
         tracking_number = self.sanitize(tracking_number)
